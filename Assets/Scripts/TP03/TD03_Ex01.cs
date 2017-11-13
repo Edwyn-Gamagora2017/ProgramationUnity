ad85631a615fe9e1d99ea62b50c3103737b2668d
Unknown <edwynluis@yahoo.com.br>
2017-11-13 15:28:09 +0100

    TP 03

---
+++ Assets/Scripts/TP03/TD03_Ex01.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TD03_Ex01 : MonoBehaviour {
+
+	// Attributes
+	[SerializeField]
+	private GameObject objectToControl;		// Object to be controlled
+
+	private float moveSpeed = 0;
+	[SerializeField]
+	private float maxSpeed = 1f;			// max speed of the object to control
+	[SerializeField]
+	private float velocityStep = 0.1f;		// step for increasing the speed when the key is pressed
+	[SerializeField]
+	private float rotationStep = 3f;		// step for increasing the rotation when the key is pressed
+
+	/*
+	 * Handling user interaction with the keyboard arrows
+	 */
+	private void handleKeyboard(){
+		// Checking Keyboard : the user can press two arrows at the same time
+		// Up Arrow
+		if( Input.GetKeyDown( KeyCode.UpArrow ) ){
+			Debug.Log ( "The user pressed the UP ARROW" );
+			// Execute movement
+			if (objectToControl != null) {
+				objectToControl.transform.Rotate (
+					Vector3.up,
+					rotationStep
+				);
+			}
+		}
+		if( Input.GetKeyDown( KeyCode.DownArrow ) ){
+			Debug.Log ( "The user pressed the DOWN ARROW" );
+			// Execute movement
+			if (objectToControl != null) {
+				objectToControl.transform.Rotate (
+					Vector3.up,
+					-rotationStep
+				);
+			}
+		}
+		if( Input.GetKeyDown( KeyCode.LeftArrow ) ){
+			Debug.Log ( "The user pressed the LEFT ARROW" );
+			moveSpeed = Mathf.Max( moveSpeed - velocityStep, 0f );
+		}
+		if( Input.GetKeyDown( KeyCode.RightArrow ) ){
+			Debug.Log ( "The user pressed the RIGHT ARROW" );
+			moveSpeed = Mathf.Min( moveSpeed + velocityStep, maxSpeed );
+		}
+
+		// Removing Component when SPACE is pressed
+		if( Input.GetKeyDown( KeyCode.Space ) ){
+			Debug.Log ( "The user pressed the Keypad ENTER" );
+			if (objectToControl != null) {
+				//Destroy( objectToControl.GetComponent<MeshRenderer>() );
+				//Destroy( objectToControl.GetComponent<BoxCollider>() );
+				Destroy( objectToControl.GetComponent<Transform>() );
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.handleKeyboard ();
+
+		if (objectToControl != null) {
+			objectToControl.transform.Translate (
+				Vector3.forward * moveSpeed * Time.deltaTime
+			);
+		}
+	}
+}
---
+++ Assets/Scripts/TP03/TD03_Ex04.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TD03_Ex04 : MonoBehaviour {
+
+	// Attributes
+	[SerializeField]						// SerializeField is used to make the variable visible in the Unity's Inspector, by keeping it private or protected. Another solution is to make the variable public
+	private GameObject objectToControl;		// Object to be controlled
+
+	[SerializeField]
+	private Color buttonDownColor;			// Color that represents the button Down Event
+	[SerializeField]
+	private Color buttonPressedColor;		// Color that represents the button Pressed Event
+	[SerializeField]
+	private Color buttonUpColor;			// Color that represents the button Up Event
+
+	[SerializeField]
+	float timeShowButtonDown = 0.1f;		// time while the color for Button Down is presented
+	float timeShowCounter = 0f;				// Counter to show key down
+	float timePressedCounter = 0f;			// Counter to store the time key is down
+
+	MeshRenderer renderer;					// Renderer
+
+	// Use this for initialization
+	void Start () {
+		renderer = objectToControl.GetComponent<MeshRenderer> ();
+		renderer.material.shader = Shader.Find ("Specular");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( objectToControl ){
+			// Change Color when keypad Enter is pressed
+			if( Input.GetKeyDown( KeyCode.KeypadEnter ) ){
+				renderer.material.color = buttonDownColor;
+				timeShowCounter = timeShowButtonDown;
+				timePressedCounter = 0f;
+			}
+			if( Input.GetKey( KeyCode.KeypadEnter ) ){
+				if (timeShowCounter > 0f) {
+					timeShowCounter -= Time.deltaTime;
+				} else {
+					timePressedCounter += Time.deltaTime;
+					// Chqnge color bqsed on pressed time
+					Color colorPressed = buttonPressedColor;
+					colorPressed.r = timePressedCounter/3f;
+					renderer.material.color = colorPressed;
+				}
+			}
+			if( Input.GetKeyUp( KeyCode.KeypadEnter ) ){
+				renderer.material.color = buttonUpColor;
+			}
+		}
+	}
+}
